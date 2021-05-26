68927f44ab7bb94ed607bd03a069572d0a2a52e1
Kianna Brandt <brandtkianna@gmail.com>
2021-05-25 20:20:22 -0700

    Completed issue-3-view-stock-details.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 var $tableLast = document.querySelectorAll('.last');
 var $tableTitle = document.querySelector('.title');
-var $totalReturn = document.querySelector('.total-return');
 var $tableData = document.querySelectorAll('td')
 
 var $form = document.querySelector('#form');
@@ -15,24 +14,43 @@ function getLastPrice() {
     }
   });
   xhr.send();
-}
+};
 
 getLastPrice();
 
-$form.addEventListener('submit', function () {
-  event.preventDefault();
-  stocks.push($form.elements[0].value);
-  $form.reset()
-  $tableTitle.textContent = stocks[stocks.length-1]
-  $tableData[2].textContent = 'Change Percentage'
-  $tableData[16].classList.add('hide')
+function getStockInfo() {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://sandbox.tradier.com/v1/markets/search?q=' + stocks[stocks.length - 1]);
   xhr.setRequestHeader('Authorization', 'Bearer GaAaHyfRiHqr4OvhZBdHrkhg4Aq4');
   xhr.setRequestHeader('Accept', 'application/json');
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
-
+    $tableData[4].textContent = xhr.response.securities.security[0].symbol;
+    $tableData[5].textContent = xhr.response.securities.security[0].description;
   });
   xhr.send();
+  xhr.onload = function getStockQuote() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://sandbox.tradier.com/v1/markets/quotes?symbols=' + $tableData[4].textContent);
+    xhr.setRequestHeader('Authorization', 'Bearer GaAaHyfRiHqr4OvhZBdHrkhg4Aq4');
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
+      $tableData[6].textContent = xhr.response.quotes.quote.change_percentage;
+      $tableData[7].textContent = xhr.response.quotes.quote.last;
+    });
+    xhr.send();
+  };
+};
+
+$form.addEventListener('submit', function () {
+  event.preventDefault();
+  stocks.push($form.elements[0].value);
+  $form.reset()
+  $tableTitle.textContent = stocks[stocks.length - 1];
+  $tableData[2].textContent = 'Change Percentage';
+  for (var i = 8; i < $tableData.length; i++) {
+    $tableData[i].classList.add('hide')
+  };
+ getStockInfo();
 });
